d6d630f7967af831e67816d51d70ed562d48baf4
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2024-11-29 11:30:17 +0530

    Added login button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,22 @@
-export default function DashboardPage() {
-  return <p>Dashboard Page</p>;
+import Image from "next/image";
+import {inter, lusitana} from "@/app/ui/fonts";
+
+export default function DashboardHome() {
+    return (
+        <div
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <div className="flex flex-col gap-4 sm:flex-col sm:gap-8 items-center">
+                    <Image src={"/tb_dalle.png"} alt="tb_dalle" width={400} height={400}/>
+                    <h1 className="text-5xl font-bold">
+                        Welcome to <span className="text-yellow-500">AI</span>mpact Diagnostics
+                    </h1>
+                </div>
+            </main>
+            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+                {/*<p className="text-sm"> © AImpact 2024</p>*/}
+                <p className={`${lusitana.className} antialiased`}> © AImpact 2024</p>
+            </footer>
+        </div>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
-import {inter, lusitana} from "@/app/ui/fonts";
+import {lusitana} from "@/app/ui/fonts";
+import DiagnosticsLogo from "@/app/ui/diagnostics_logo";
+import {PowerIcon} from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -7,10 +9,16 @@ export default function Home() {
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <div className="flex flex-col gap-4 sm:flex-col sm:gap-8 items-center">
-                    <Image src={"/tb_dalle.png"} alt="tb_dalle" width={400} height={400}/>
+                    <DiagnosticsLogo/>
                     <h1 className="text-5xl font-bold">
                         Welcome to <span className="text-yellow-500">AI</span>mpact Diagnostics
                     </h1>
+                    <button
+                        className="text-yellow-500 hover:text-yellow-600 flex flex-row h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                        <Link href={"/dashboard"}>
+                            <div className="hidden md:block">Login to get started</div>
+                        </Link>
+                    </button>
                 </div>
             </main>
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
