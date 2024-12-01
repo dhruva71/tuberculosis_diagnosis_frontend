51fbd16541291228f55a6350d12c25793fede20c
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2024-12-01 12:29:58 +0530

    Switching to postgres

--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -3,3 +3,11 @@ export type XrayData = {
     class: string;
     timestamp: string;
 };
+
+export type User = {
+    id: number;
+    name: string;
+    email: string;
+    image: string;
+    password: string;
+};
