02fe7b6c2e1b777a51b7eed0293b5d2bb0cd1e0d
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2024-12-01 13:24:53 +0530

    Enabled sign in

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import {inter} from "@/app/ui/fonts";
-// import { signOut } from '@/login';
+import { signOut } from '@/auth';
 
 export default function SideNav() {
   return (
@@ -18,7 +18,7 @@ export default function SideNav() {
         <div className="hidden h-auto w-full grow rounded-md bg-gray-700 md:block"></div>
         <form action={async () => {
             'use server';
-            // await signOut();
+            await signOut();
           }}>
           <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium bg-gray-300 text-black hover:bg-sky-300 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
