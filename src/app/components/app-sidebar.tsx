2adb17688610d02043e1e266a526043427b7f3ac
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2024-12-02 14:05:28 +0530

    Signout button added to sidebar

--- src/app/components/app-sidebar.tsx
+++ src/app/components/app-sidebar.tsx
@@ -1,52 +1,58 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import {Calendar, Home, Inbox, Search, Settings} from "lucide-react"
+import SignOutButton from '@/app/ui/dashboard/SignOutButton';
 
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
 } from "@/components/ui/sidebar"
 
 // Menu items.
 const items = [
-  {
-    title: "Home",
-    url: "/dashboard",
-    icon: Home,
-  },
-  {
-    title: "Tuberculosis Diagnosis",
-    url: "/dashboard/tuberculosis_diagnosis",
-    icon: Inbox,
-  },
+    {
+        title: "Home",
+        url: "/dashboard",
+        icon: Home,
+    },
+    {
+        title: "Tuberculosis Diagnosis",
+        url: "/dashboard/tuberculosis_diagnosis",
+        icon: Inbox,
+    },
 ]
 
 export function AppSidebar() {
-  return (
-    <Sidebar>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>AImpact Diagnostics</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
-  )
+    return (
+        <Sidebar>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>AImpact Diagnostics</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon/>
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                            <SidebarMenuItem key={"signout"}>
+                                <SidebarMenuButton asChild>
+                                    <SignOutButton/>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    )
 }
