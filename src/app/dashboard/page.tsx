b4fdd200190731e3ddf68272ac739c78a546ffa0
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2024-12-02 20:49:49 +0530

    Optimized imports

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,6 @@
 import Image from "next/image";
-import {lusitana} from "@/app/ui/fonts";
 import Link from "next/link";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+import {Card, CardContent, CardDescription, CardHeader, CardTitle,} from "@/components/ui/card"
 
 
 export default function DashboardHome() {
