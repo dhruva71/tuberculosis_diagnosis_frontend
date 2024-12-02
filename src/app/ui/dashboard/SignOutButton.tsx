3170bc24b393bffb4a7875035209ac83dc6d7643
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2024-12-02 14:40:52 +0530

    Improved signout button

--- src/app/ui/dashboard/SignOutButton.tsx
+++ src/app/ui/dashboard/SignOutButton.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 import { signOut } from 'next-auth/react';
 import { useState } from 'react';
 import { Button } from "@/components/ui/button";
 
-export default function SignOutButton() {
+export default function SignOutLink() {
     const [error, setError] = useState('');
 
     const handleSignOut = async () => {
@@ -19,16 +20,21 @@ export default function SignOutButton() {
 
     return (
         <>
-            <Button
-                type="button"
-                onClick={handleSignOut}
-                className="flex items-center justify-center text-sm font-medium md:justify-start w-full"
-                variant="secondary"
-                size="lg"
+            <Link
+                href="#"
+                onClick={(e) => {
+                    e.preventDefault(); // Prevent default link behavior
+                    handleSignOut();
+                }}
+                className="flex h-[48px] items-center justify-center gap-2 text-sm font-medium md:justify-center w-full"
             >
-                <PowerIcon className="w-5 text-muted-foreground" />
-                <span className="hidden md:block text-foreground">Sign Out</span>
-            </Button>
+                <Button variant="secondary" size="lg" asChild>
+                    <span className="flex items-center gap-2 w-full">
+                        <PowerIcon className="text-muted-foreground" />
+                        <span className="hidden md:block text-foreground">Sign Out</span>
+                    </span>
+                </Button>
+            </Link>
             {error.length > 0 && (
                 <p className="text-destructive mt-2 text-sm">
                     {error}
