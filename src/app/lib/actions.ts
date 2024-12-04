ccc3738a358741422c37a108b420d455bb076ec5
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2024-12-04 11:36:03 +0530

    Added redirect in authentication action

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -8,6 +8,7 @@ export async function authenticate(
   prevState: string | undefined,
   formData: FormData,
 ) {
+  formData.set('redirectTo', '/dashboard');
   try {
     await signIn('credentials', formData);
   } catch (error) {
